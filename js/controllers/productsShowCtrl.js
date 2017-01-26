8f4036b426c2b6590dec06a67ddee0c8b9ffcfc8
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-01-26 16:15:31 -0500

    Implement product purchasing

--- js/controllers/productsShowCtrl.js
+++ js/controllers/productsShowCtrl.js
@@ -3,7 +3,13 @@ VikingStore.controller("ProductsShowCtrl",
   ["$scope", "$stateParams", "productService",
 
   function($scope, $stateParams, productService){
+
     $scope.product = productService.find($stateParams.id);
+
+    $scope.addToCart = function addToCart(product, quantity){
+      // TODO
+    };
+
   }
 
 ]);
