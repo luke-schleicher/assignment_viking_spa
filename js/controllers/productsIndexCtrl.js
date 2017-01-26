c1f4d5448d1003c3eeb661d23a2e101204b69acb
luke-schleicher <luke.j.schleicher@gmail.com>
2017-01-26 16:37:33 -0600

    added flash

--- js/controllers/productsIndexCtrl.js
+++ js/controllers/productsIndexCtrl.js
@@ -1,14 +1,18 @@
 
 VikingStore.controller("ProductsIndexCtrl",
 
-  ["$scope", "productService", "categoryService", "cartService",
+  ["$scope", "$timeout", "productService", "categoryService", "cartService",
 
-    function($scope, productService, categoryService, cartService){
+    function($scope, $timeout, productService, categoryService, cartService){
 
       $scope.products = productService.all();
       $scope.categories = categoryService.all();
       $scope.incrementQuantity = function incrementQuantity(product){
         cartService.incrementQuantity(product);
+        $scope.message = "Added " + product.name + " to cart";
+        $timeout(function(){
+          $scope.message = null;
+        }, 3000);
       };
 
     }
